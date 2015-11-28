1abd83ad71a13cb56dd3c70af1f208599b36ee29
hanks-zyh <zhangyuhan2014@gmail.com>
2015-11-29 02:02:17 +0800

    finish callback

--- app/src/main/java/com/hanks/rxsearch/MainActivity.java
+++ app/src/main/java/com/hanks/rxsearch/MainActivity.java
@@ -6,16 +6,22 @@ import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
 import com.jakewharton.rxbinding.widget.RxTextView;
 
+import java.util.ArrayList;
+
 import butterknife.Bind;
+import butterknife.ButterKnife;
 import retrofit.RestAdapter;
+import retrofit.converter.GsonConverter;
 import retrofit.http.GET;
 import retrofit.http.Query;
+import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action0;
 import rx.functions.Action1;
 import rx.functions.Func1;
-import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,41 +31,68 @@ public class MainActivity extends AppCompatActivity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         RestAdapter retrofit = new RestAdapter.Builder().setEndpoint("https://suggest.taobao.com")
                 .setLogLevel(RestAdapter.LogLevel.FULL)
+                .setConverter(new GsonConverter(new Gson()))
                 .build();
 
         final SearchService service = retrofit.create(SearchService.class);
 
         RxTextView.textChanges(et_keyword)
-                .subscribeOn(Schedulers.io())
-                .flatMap(new Func1<CharSequence, rx.Observable<String>>() {
-                    @Override public rx.Observable<String> call(CharSequence keyword) {
-                        return service.searchProdcut("utf-8", (String) keyword);
+                .filter(new Func1<CharSequence, Boolean>() {
+                    @Override public Boolean call(CharSequence charSequence) {
+                        return charSequence.length() > 0;
+                    }
+                })
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .flatMap(new Func1<CharSequence, Observable<Data>>() {
+                    @Override public Observable<Data> call(CharSequence charSequence) {
+                        return service.searchProdcut("utf-8", charSequence.toString());
+                    }
+                })
+                .doOnSubscribe(new Action0() {
+                    @Override public void call() {
+                        Log.i("doOnSubscribe","clear");
+                        tv_result.setText("");
+                    }
+                })
+                .map(new Func1<Data, ArrayList<ArrayList<String>>>() {
+                    @Override public ArrayList<ArrayList<String>> call(Data data) {
+                        return data.result;
+                    }
+                })
+                .flatMap(new Func1<ArrayList<ArrayList<String>>, Observable<ArrayList<String>>>() {
+                    @Override public Observable<ArrayList<String>> call(ArrayList<ArrayList<String>> arrayLists) {
+                        return Observable.from( arrayLists);
+                    }
+                })
+                .flatMap(new Func1<ArrayList<String>, Observable<String>>() {
+                    @Override public Observable<String> call(ArrayList<String> strings) {
+                        return Observable.from(strings);
                     }
                 })
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Action1<String>() {
-                    @Override public void call(String s) {
-                        Log.d("result", s);
-                        showpop(s);
-                    }
-                }, new Action1<Throwable>() {
-                    @Override public void call(Throwable throwable) {
-                        Log.d("error", throwable.getMessage());
+                    @Override public void call(String charSequence) {
+                        showpop(charSequence);
                     }
                 });
 
     }
 
     private void showpop(String result) {
-        tv_result.setText(result);
+        tv_result.append(result);
     }
 
     interface SearchService {
 
-        @GET("/sug") rx.Observable<String> searchProdcut(@Query("code") String code, @Query("q") String keyword);
+        @GET("/sug") Observable<Data> searchProdcut(@Query("code") String code, @Query("q") String keyword);
+    }
+
+    class Data {
+        public ArrayList<ArrayList<String>> result;
     }
 
     //    https://suggest.taobao.com/sug?code=utf-8&q=%E6%89%8B%E6%9C%BA
