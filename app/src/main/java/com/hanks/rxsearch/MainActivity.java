097154cdaaecef1e5e4365212b8f12c7bf62ace8
hanks-zyh <zhangyuhan2014@gmail.com>
2015-11-29 14:06:39 +0800

    use debounce to filter unnecessary net request

--- app/src/main/java/com/hanks/rxsearch/MainActivity.java
+++ app/src/main/java/com/hanks/rxsearch/MainActivity.java
@@ -2,7 +2,6 @@ package com.hanks.rxsearch;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -10,6 +9,7 @@ import com.google.gson.Gson;
 import com.jakewharton.rxbinding.widget.RxTextView;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -19,9 +19,9 @@ import retrofit.http.GET;
 import retrofit.http.Query;
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action0;
 import rx.functions.Action1;
 import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,38 +41,49 @@ public class MainActivity extends AppCompatActivity {
         final SearchService service = retrofit.create(SearchService.class);
 
         RxTextView.textChanges(et_keyword)
+                // 每150毫秒发射一次事件流， 150毫秒之内的被过滤掉
+                .debounce(150, TimeUnit.MILLISECONDS)
                 .filter(new Func1<CharSequence, Boolean>() {
                     @Override public Boolean call(CharSequence charSequence) {
+                        // 清空搜索出来的结构
+                        tv_result.setText("");
+                        //当 EditText 中文字大于0的时候
                         return charSequence.length() > 0;
                     }
                 })
+                // 上面的对 tv_result 的操作需要在主线程
                 .subscribeOn(AndroidSchedulers.mainThread())
                 .flatMap(new Func1<CharSequence, Observable<Data>>() {
                     @Override public Observable<Data> call(CharSequence charSequence) {
+                        // 搜索
                         return service.searchProdcut("utf-8", charSequence.toString());
                     }
                 })
-                .doOnSubscribe(new Action0() {
-                    @Override public void call() {
-                        Log.i("doOnSubscribe","clear");
-                        tv_result.setText("");
-                    }
-                })
+                // 网络操作在io线程
+                .subscribeOn(Schedulers.io())
+                //将 data 转换成 ArrayList<ArrayList<String>>
                 .map(new Func1<Data, ArrayList<ArrayList<String>>>() {
                     @Override public ArrayList<ArrayList<String>> call(Data data) {
                         return data.result;
                     }
                 })
+                // 将 ArrayList<ArrayList<String>> 中每一项提取出来 ArrayList<String>
                 .flatMap(new Func1<ArrayList<ArrayList<String>>, Observable<ArrayList<String>>>() {
                     @Override public Observable<ArrayList<String>> call(ArrayList<ArrayList<String>> arrayLists) {
-                        return Observable.from( arrayLists);
+                        return Observable.from(arrayLists);
                     }
                 })
-                .flatMap(new Func1<ArrayList<String>, Observable<String>>() {
-                    @Override public Observable<String> call(ArrayList<String> strings) {
-                        return Observable.from(strings);
+                .filter(new Func1<ArrayList<String>, Boolean>() {
+                    @Override public Boolean call(ArrayList<String> strings) {
+                        return strings.size() >= 2;
                     }
                 })
+                .map(new Func1<ArrayList<String>, String>() {
+                    @Override public String call(ArrayList<String> strings) {
+                        return "[商品名称:" + strings.get(0) + ", ID:" + strings.get(1) + "]\n";
+                    }
+                })
+                //
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Action1<String>() {
                     @Override public void call(String charSequence) {
